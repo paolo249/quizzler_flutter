8691aa203ccfc97626cda03585c460ba7dcaa36e
Paolo <paologarciaang@gmail.com>
2024-06-09 20:13:27 -0700

    Add rFlutter_alert package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 // import 'package:quizzler_flutter/quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'quiz_brain.dart';
 
 void main() {
