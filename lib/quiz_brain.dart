eba5072940e7d9a046e37d256344d56a6156306f
Paolo <paologarciaang@gmail.com>
2024-06-09 21:30:32 -0700

    create method isFinished() in quiz_brain.dart to check to see if we are at the end of the quiz

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -50,13 +50,18 @@ class QuizBrain {
 
     //TODO: Step 3 - Create a method called is isFinished() here that checks to see if we are at the end of the quiz
     bool isFinished(){
+      bool? isFalse = false;
       if(_questionNumber == _questionBank.length-1){
         print("end");
+        isFalse = true;
+        return isFalse;
       }
       else{
         print("not yet finished");
+        isFalse = false;
+        return isFalse;
       }
     
-      return true;
+      
     }
 }
